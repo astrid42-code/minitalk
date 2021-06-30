1288415e59f7efd18bc25f98bf5f1a992e30bc57
Astrid GAULTIER <asgaulti@e1r8p11.42.fr>
2021-06-30 17:57:41 +0200

    descr main

--- srcs/ft_client.c
+++ srcs/ft_client.c
@@ -6,9 +6,22 @@
 /*   By: asgaulti <asgaulti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 20:42:44 by astridgault       #+#    #+#             */
-/*   Updated: 2021/06/24 12:21:48 by asgaulti         ###   ########.fr       */
+/*   Updated: 2021/06/30 17:53:38 by asgaulti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+int	main(int ac, char **av)
+{
+	int	pid;
+
+	if (ac == 3)
+	{
+		
+		//recevoir le signal du serveur et l'envoyer aux fcts qui traduisent le binaire recu en char / int ...
+	}
+	else
+		//msg erreur?
+	return (0);
+}
--- srcs/ft_server.c
+++ srcs/ft_server.c
@@ -6,9 +6,20 @@
 /*   By: asgaulti <asgaulti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 20:42:27 by astridgault       #+#    #+#             */
-/*   Updated: 2021/06/24 12:21:40 by asgaulti         ###   ########.fr       */
+/*   Updated: 2021/06/30 17:57:11 by asgaulti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		// verifier que av[1] = nbr?
+		// envoyer l'arg[2] en binaire au client
+	}
+	else
+		//msg erreur?
+	return (0);
+}
