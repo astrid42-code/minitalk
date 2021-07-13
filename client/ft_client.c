ccf3642c501a8bfef4935dc671e2f0d1351df1f1
asgaulti <asgaulti@student.42.fr>
2021-07-13 17:55:41 +0200

    client2

--- client/ft_client.c
+++ client/ft_client.c
@@ -16,13 +16,12 @@ int	main(int ac, char **av)
 {
 	pid_t	pid;
 	int		j;
-	//int		nbr;
 
 	pid = 0;
 	if (ac != 3)
 	{
 		write(1, "Wrong number of arguments\n", 26);
-		EXIT_FAILURE;
+		exit (EXIT_FAILURE);
 	}
 	else
 	{
@@ -32,18 +31,20 @@ int	main(int ac, char **av)
 			if (ft_isdigit(av[1][j]) == 0)
 			{
 				write(1, "First argument is not a number\n", 31);
-				return (1); // ou exit?
+				exit (EXIT_FAILURE);
 			}
 			j++;
 		}
 		pid = ft_atoi(av[1]);
+		printf("PID = %d av = %s\n", pid, av[1]);
 	}
+	// comment vérifier que c le meme pid que celui du server??
 	if (pid <= 1)
 	{
 		write(1, "Wrong PID\n", 10);
-		EXIT_FAILURE;
+		exit (EXIT_FAILURE);
 	}
-	ft_treat_arg(pid, av[2]);
+	ft_find_bit(pid, av[2]);
 	return (0);
 }
 
@@ -53,28 +54,28 @@ void	ft_find_bit(int pid, char *str)
 	int count;
 	int i;
 
-	count = 0;
 	i = 0;
+	bit = 0;
 	while (str[i])
 	{
-		c = 0;
-		while (c < 8)
+		count = 0;
+		while (count < 8)
 		{
-			bit |= (str[i] >> c++);
-			ft_send_sig(pid, bit);
+			bit |= (str[i] >> count++);
+			ft_send_signal(pid, bit);
 		}
 		i++;
 	}
 }
 
-void	ft_send_sig(int pid, int bit)
+void	ft_send_signal(int pid, int bit)
 {
 	if (bit == 0)
 	{
 		if (kill(pid, SIGUSR1) == -1)
 		{
 			write(1, "Sigusr1 error\n", 14);
-			EXIT_FAILURE;
+			exit (EXIT_FAILURE);
 		}
 	}
 	if (bit == 1)
@@ -82,7 +83,7 @@ void	ft_send_sig(int pid, int bit)
 		if (kill(pid, SIGUSR2) == -1)
 		{
 			write(1, "Sigusr2 error\n", 14);
-			EXIT_FAILURE;
+			exit (EXIT_FAILURE);
 		}
 	}
 }
--- include/minitalk.h
+++ include/minitalk.h
@@ -19,16 +19,17 @@
 # include <unistd.h>
 # include <stdio.h>
 
-int	main(int ac, char **av);
+int     main(int ac, char **av);
+void	ft_find_bit(int pid, char *str);
+void	ft_send_signal(int pid, int bit);
 
 // utils
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isdigit(char c);
-int	ft_atoi(const char *str);
+int	    ft_isalnum(int c);
+int	    ft_isalpha(int c);
+int	    ft_isdigit(char c);
+int	    ft_atoi(const char *str);
 void	ft_putnbr(int nb);
-int	ft_strlen(char *str);
+int	    ft_strlen(char *str);
 void	ft_putchar(char c);
-void	ft_find_bit(int pid, char *str);
 
 #endif
